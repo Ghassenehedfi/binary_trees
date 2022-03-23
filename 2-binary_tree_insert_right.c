c9d9fc5abd1ada6609b72d1f4dfe4a7826d32674
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-03-23 20:30:46 +0100

    insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,7 +4,7 @@
  * binary_tree_insert_right - function that inserts a node as the right-child.
  * @parent: A pointer to the parent of the node to create.
  * @value: The value to put in the new node.
- * Return: If an error occurs - NULL else  pointer to the new node
+ * Return: If an error occurs - NULL else
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
